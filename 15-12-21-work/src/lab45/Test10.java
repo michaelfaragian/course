909687f694bc18db82b257fd3ef5045d41996ab2
jbt <jbt@40X-XX>
2021-12-15 16:31:24 +0200

    a

--- 15-12-21-work/src/lab45/Test10.java
+++ 15-12-21-work/src/lab45/Test10.java
@@ -3,37 +3,23 @@ package lab45;
 public class Test10 {
 
 	public static void main(String[] args) {
-		
+		int b = 0;
 		int a = (int)(Math.random()*10_001);
-		int y = a;
-		System.out.println(a);
-		int x =a;
-		while (a > 9) {
-			dig ++;
-			a = (int) (a / 10);			
+		int x = a;
+		while (a != 0) {
+			b = a % 10;	
+			b = b + (b*10);
+			a = (int)( a / 10);
 		}
-		System.out.println("the number of digit: " + dig);
-		
-		while (a > 9) {
-			a = (int) (a / 10);
-			
-		}System.out.println("the left number is: " + a);
-		
-		while (x != 0) {
-			 s = s + (x % 10) ;
-			x = (int)(x / 10);
+		System.out.println(x);
+		System.out.println(b);
+		if (x == b) {
+			System.out.println("palindrom");
+		} else
+			System.out.println("no palindrom");
+		{
+
 		}
-		System.out.println("the sum of the digits: " + s);
-		
-		
-		System.out.print("the oposite number: " );
-		while (y > 0) {
-			b = (y % 10);
-			y = (int)(y / 10);
-			System.out.print(b);
-		}
-		
-		
 		
 		
 			   
