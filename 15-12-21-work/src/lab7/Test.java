ac99c6e3945a38ac6006d93c197a5a6dc0145911
jbt <jbt@40X-XX>
2021-12-16 16:30:23 +0200

    a

---
+++ 15-12-21-work/src/lab7/Test.java
@@ -0,0 +1,20 @@
+package lab7;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int c = 0;
+
+		char arr[] = {'a','b','c','a','b','d','r','c'};
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 'a' || arr[i] == 'c') {
+				c++;
+				System.out.println(c);
+			}
+		}
+		
+		
+		
+	}
+
+}
