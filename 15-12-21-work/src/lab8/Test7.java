e63926c8e0278878fbbbc69c1542b25ef5fa196e
micha <micha@LAPTOP-69GQ7UIQ>
2021-12-21 22:33:36 +0200

    aa

--- 15-12-21-work/src/lab8/Test7.java
+++ 15-12-21-work/src/lab8/Test7.java
@@ -1,24 +1,24 @@
 package lab8;
 
-public class Test6 {
+public class Test7 {
 
 	public static void main(String[] args) {
 
 		int c = 0;
-		int max = 0;
+		int min = 101;
 		
 		int[] arr = new int[10];
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = (int)(Math.random()*101);
 			System.out.print(arr[i] + ", ");
-			if(arr[i] > max) {
-				max = arr[i];
+			if(arr[i] < min) {	
+				min = arr[i];
 				c = i +1;
 			}
 		}
 		System.out.println();
 		System.out.println("index: " + c);		
-		System.out.println("max number: " + max);
+		System.out.println("min number: " + min);
 	}
 
 }
