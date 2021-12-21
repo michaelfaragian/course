db51d7752c65b0f776ccaab5d99dbeba9c99e01f
jbt <jbt@40X-XX>
2021-12-21 16:30:00 +0200

    a

--- 21-12-21-exceptions/src/b/collections/Demo3Milon.java
+++ 21-12-21-exceptions/src/b/collections/Demo3Milon.java
@@ -2,6 +2,7 @@ package b.collections;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Demo3Milon {
 
@@ -9,16 +10,23 @@ public class Demo3Milon {
 
 		Map<String, String> map = new HashMap<>();
 		map.put("java", " a programming lanuage");
+		map.put("dog", " an animal that barks");
 		map.put("table", " a furniture");
 		map.put("dog", " an animal that barks");
-		
+
 		System.out.println(map);
-		
+
 		for (String key : map.keySet()) {
 			System.out.println(key + ": " + map.get(key));
 		}
-		
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter word: ");
+		String key = sc.nextLine();
+		if(map.containsKey(key)) {
+			System.out.println("definition");
+			
+		}
+
 	}
 
 }
